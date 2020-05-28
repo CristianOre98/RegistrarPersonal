a5572af8d191eba119ba43c7199b4b73855fb0de
CristianOre98 <crisvlaore98@gmail.com>
2020-05-27 19:31:13 -0600

    he agregado la clase Recibir

---
+++ RegistrarPersonalMVC/src/java/controlador/Recibir.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+
+import modelo.Persona;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Recibir extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        String dui = request.getParameter("txtDui");
+        String apellidos =  request.getParameter("txtApellidos");
+        String nombres = request.getParameter("txtNombres");
+        
+        Persona person = new Persona(); //Al crear este objeto automaticamente se conecta a la BD
+        person.setDui(dui);
+        person.setApellido(apellidos);
+        person.setNombres(nombres);
+        
+        if(person.insertarDatos() == true){//Recuerda que era booleana
+            request.getRequestDispatcher("exito.jsp").forward(request, response);
+        }else{
+             request.getRequestDispatcher("noexito.jsp").forward(request, response);
+        }
+        
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
